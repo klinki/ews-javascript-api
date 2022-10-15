73104c9ee6270f9dbeb4da037c0e89bdb6ca8010
David Klingenberg <dklingenberg@gmail.com>
2022-10-15 23:09:17 +0200

    Use XHRBrowser

--- src/js/ConfigurationApi.ts
+++ src/js/ConfigurationApi.ts
@@ -1,8 +1,6 @@
 import { XHRFactory } from "./XHRFactory";
 import { PromiseConstructor, ConfigurePromise } from "./Promise";
 import { IXHROptions, IXHRApi } from "./Interfaces";
-import { FetchOptions } from 'fetch';
-import { XHRDefault } from './XHRDefault';
 
 export class ConfigurationApi {
 
@@ -10,9 +8,9 @@ export class ConfigurationApi {
         XHRFactory.xhrHelper = xhrApi;
     }
 
-    static SetXHROptions(fetchOptions: FetchOptions) {
-        XHRDefault.defaultOptions = fetchOptions;
-    }
+    // static SetXHROptions(fetchOptions: FetchOptions) {
+    //     XHRDefault.defaultOptions = fetchOptions;
+    // }
 
     static ConfigurePromise(promise: PromiseConstructor) {
         ConfigurePromise(promise);
--- src/js/XHRFactory.ts
+++ src/js/XHRFactory.ts
@@ -1,18 +1,18 @@
 ﻿import { IXHROptions, IXHRApi } from "./Interfaces";
-import { XHRDefault } from "./XHRDefault"
+import { XHRBrowser } from "./XHRBrowser";
 
 export class XHRFactory {
 
 	static xhrHelper: IXHRApi;
 	static get XHRApi() {
 		if (typeof this.xhrHelper === 'undefined' || this.xhrHelper === null) {
-			this.xhrHelper = new XHRDefault();
+			this.xhrHelper = new XHRBrowser();
 		}
 		return this.xhrHelper;
 	}
 
 	public static newXHRApi() {
 		console.warn("depricated, import and use \"new XHRDefault(options?)\" instead")
-		return new XHRDefault();
+		return new XHRBrowser();
 	}
 }
