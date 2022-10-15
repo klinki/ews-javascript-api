677e83d4f79d63aa201397c47183db7cdffe20ab
David Klingenberg <dklingenberg@gmail.com>
2022-10-16 00:35:50 +0200

    Remove promise typings
    Use platform default ones

--- src/js/Promise.ts
+++ src/js/Promise.ts
@@ -1,24 +1,3 @@
-﻿import { NotSupportedException } from "./Exceptions/NotSupportedException";
-
-export type PromiseConstructor<T=unknown> = Promise<T>;
-export let Promise: PromiseConstructor = window.Promise as any;
-
-// class PromiseType<T> {
-//     constructor() {
-//         throw new NotSupportedException("Promise implementation missing.\nPlease use ConfigurePromise function to assign Promise Object\n" +
-//             "bluebird: ConfigurePromise(require('bluebird))\n" +
-//             "Q: ConfigurePromise(require('q').Promise)\n" +
-//             "winjs-node: ConfigurePromise(require('winjs-node').Promise)")
-//     }
-// }
-
-// try {
-//     var promise = require('bluebird');
-//     Promise = promise;
-// } catch (e) {
-//     Promise = <any>PromiseType;
-// }
-
-export function ConfigurePromise(promise: PromiseConstructor) {
-    Promise = promise;
+﻿export function ConfigurePromise(promise: PromiseConstructor) {
+    window.Promise = promise;
 }
